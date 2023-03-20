431e8646416582945167cb08708914317c94538f
Marcus Fadrigalan <marcusfadrigalan@gmail.com>
2023-03-20 15:56:44 +0800

    Add activity code

---
+++ indexActivity.js
@@ -0,0 +1,16 @@
+console.log('Hello, Welcome to De La Salle University!');
+
+
+let fName = 'Marcus';
+let lName = 'Fadrigalan';
+let age = 19;
+let hobbies = ['Playing different intruments', 'Window shopping', 'Solving different puzzles', 'Playing games', 'Dancing'];
+let homeAddress = {
+houseNumber: 1, Street: 'Tagaytay', village: 'villagename', barangay: 'neogan', province: 'Cavite', zipCode: 1234
+};
+
+console.log(fName);
+console.log(lName);
+console.log(age);
+console.log(hobbies);
+console.log(homeAddress);
